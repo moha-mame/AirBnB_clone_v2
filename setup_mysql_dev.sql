a0fa32f7c77cfdbbb28e2e5d5fd17c6d3ce9ff31
moha-mame <>
2023-03-21 11:26:14 +0000

    AirBnB_clone_v2

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,11 +1,7 @@
--- Create the database hbnb_dev_db if it doesn't exist
+-- prepares a MySQL server for the project
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
--- Create the user hbnb_dev if it doesn't exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
--- Grant all privileges on the hbnb_dev_db to hbnb_dev
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
--- Grant SELECT privilege on the performance_schema to hbnb_dev
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
