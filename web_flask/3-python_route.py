b70206ba3fb7ebd0ff9108728988f9ffa3b4c1cc
moha-mame <>
2023-04-23 08:03:02 +0100

    web_flask

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -18,7 +18,7 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    return f'C {text.replace("_", " ")}'
+    return 'C ' + text.replace('_', ' ')
 
 
 @app.route('/python/', strict_slashes=False)
