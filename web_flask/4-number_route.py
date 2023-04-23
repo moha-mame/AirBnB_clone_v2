c35e1e839171f48772c178fc04179181c8cc9c6e
moha-mame <>
2023-04-23 08:27:01 +0100

    web_flask

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Start web application with two routings
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_number(n):
+    return str(n) + ' is a number'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
