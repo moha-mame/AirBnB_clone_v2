6fdfbfe031c904d1960ada60f33245e40de1b7ef
moha-mame <>
2023-04-23 09:08:00 +0100

    web_flask

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -5,51 +5,36 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello():
-    """Return string when route queried
-    """
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Return string when route queried
-    """
     return 'HBNB'
 
 
-@app.route('/c/<text>')
-def c_is_fun(text):
-    """Return reformatted text
-    """
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
     return 'C ' + text.replace('_', ' ')
 
 
-@app.route('/python/')
-@app.route('/python/<text>')
-def python_with_text(text='is cool'):
-    """Reformat text based on optional variable
-    """
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
     return 'Python ' + text.replace('_', ' ')
 
 
-@app.route('/number/<int:n>')
-def number(n=None):
-    """Allow request if path variable is a valid integer
-    """
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_number(n):
     return str(n) + ' is a number'
 
 
-@app.route('/number_template/<int:n>')
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    """Retrieve template for request
-    """
-    path = '5-number.html'
-    return render_template(path, n=n)
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
