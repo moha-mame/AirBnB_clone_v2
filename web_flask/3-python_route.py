d7472959f4414298701f4b8076b3fd2e2984cf2f
moha-mame <>
2023-04-23 08:01:09 +0100

    web_flask

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -24,7 +24,7 @@ def c_text(text):
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text='is cool'):
-    return f'Python {text.replace("_", " ")}'
+    return 'Python ' + text.replace('_', ' ')
 
 
 if __name__ == '__main__':
