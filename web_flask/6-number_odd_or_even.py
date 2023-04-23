158daf8f8d8610590f1e8b2faf13d1b928768553
moha-mame <>
2023-04-23 11:23:57 +0100

    web_flask

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -37,11 +37,10 @@ def number_template(n):
     return render_template('5-number.html', n=n)
 
 
-@app.route('/number_odd_or_even/<int:n>')
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
-
-    return render_template('6-number_odd_or_even.html', n=n, odd_or_even="even" if n % 2 == 0 else "odd")
-
+    return render_template('6-number_odd_or_even.html', n=n,
+                            odd_or_even="even" if n % 2 == 0 else "odd")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
