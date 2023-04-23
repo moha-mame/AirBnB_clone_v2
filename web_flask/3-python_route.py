fdf7f7afd4a64f7ff4d2fdb77a8ff2ef946b2996
moha-mame <>
2023-04-23 07:54:23 +0100

    web_flask

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -6,35 +6,26 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    """Return string when route queried
-    """
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Return string when route queried
-    """
     return 'HBNB'
 
 
-@app.route('/c/<text>')
-def c_is_fun(text):
-    """Return reformatted text
-    """
-    return 'C ' + text.replace('_', ' ')
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    return f'C {text.replace("_", " ")}'
 
 
-@app.route('/python/')
-@app.route('/python/<text>')
-def python_with_text(text='is cool'):
-    """Reformat text based on optional variable
-    """
-    return 'Python ' + text.replace('_', ' ')
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    return f'Python {text.replace("_", " ")}'
 
 
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
