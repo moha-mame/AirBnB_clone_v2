82534ee0326d8810afe9f9253b8a42d699daf0c4
moha-mame <>
2023-04-26 09:40:47 +0100

    web_flask

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    states = storage.all(State).values()
+    states = storage.all('State').values()
     states = sorted(states, key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
