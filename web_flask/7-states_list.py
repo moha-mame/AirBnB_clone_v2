c5d65fae6b9ebf8a05913e4e25d01be66d19ab50
moha-mame <>
2023-04-26 08:36:25 +0100

    web_flask

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,25 +2,24 @@
 """
 Starting my first Flask web application
 """
-from flask import Flask, render_template
-import sys
-sys.path.insert(1, '/AirBnB_clone_v2')
 from models import storage
-
+from models.state import State
+from os import environ
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    states = storage.all('State').values()
-    states = sorted(states, key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states)
-
-
 @app.teardown_appcontext
-def teardown_db(exception):
+def close_db(error):
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route('/states_list', strict_slashes=False)
+def list_states():
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
