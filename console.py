b4eceb3eec5387d1202aaf59b20648ebb4088d6a
moha-mame <>
2023-04-28 07:29:52 +0100

    AirBnB_clone_v2

--- console.py
+++ console.py
@@ -11,8 +11,7 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-import shlex  # for splitting the line along spaces except in double quotes
-
+import shlex
 classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
 
@@ -68,7 +67,7 @@ class HBNBCommand(cmd.Cmd):
             return False
         print(instance.id)
         instance.save()
-    
+
     def do_show(self, arg):
         """Prints an instance as a string based on the class and id"""
         args = shlex.split(arg)
